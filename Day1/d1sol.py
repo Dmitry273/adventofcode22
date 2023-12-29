e1812219989a0652aed8fe948013be72a12d4d38
Catnee <ste01a01@gmail.com>
2023-12-29 08:43:27 +0400

    both

--- Day1/d1sol.py
+++ Day1/d1sol.py
@@ -1,1 +1,10 @@
-print('test')
+cals = []
+local = 0
+for line in open('Day1/d1.txt', 'r'):
+    if line[:-1]: local += int(line[:-1])
+    else:
+        cals.append(local)
+        local = 0
+cals.sort(reverse= True)
+print(cals[0])
+print(sum(cals[0:3]))
